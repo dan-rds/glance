ca46c530f5a36097d16dff3ba94eb5e74f88d87b
danieldrichards <daniel@moversandshakers.mobi>
2018-06-22 00:47:48 +0000

    Regular commit by blc00 on the digilab network at Fri Jun 22 00:47:48 UTC 2018

--- run_glance.sh
+++ run_glance.sh
@@ -4,7 +4,7 @@ echo $HOSTNAME
 netw=`/home/obs/bin/whereami`
 
 python create_report.py
-create_tree_data.py
+python create_tree_data.py
 rm -f MegaSAS.log
 git add *
 git commit -m "Regular commit by $HOSTNAME on the ${netw} network at `date`"
