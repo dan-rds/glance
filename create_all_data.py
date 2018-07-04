73b39388f7b2e9f803a342afc463055a988c64bb
daniel <ddrichar@ucc.edu>
2018-07-04 16:43:56 -0700

    speeding things up

--- create_all_data.py
+++ create_all_data.py
@@ -261,8 +261,8 @@ gpu_yaml, gpu_csv, gpu_tree = get_gpus()
 cpu_yaml, cpu_csv, cpu_tree = get_cpus()
 mem_yaml, mem_csv, mem_tree = get_mem()
 disk_yaml, disk_csv, disk_tree = get_disks()
-#print(hostname)
-print(type(disk_yaml))
+print(hostname)
+print(disk_yaml)
 for i in disk_yaml["Disks"]:
     for k,v in i.iteritems():
         print(k , v)
